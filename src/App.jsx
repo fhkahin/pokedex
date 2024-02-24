d13eaf820c0ba92e58ad04b043044e010d9e4ea1
Fathi Kahin <fhkahin@gmail.com>
2024-02-24 12:07:26 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to the Pokedex</h1>
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Pokemon" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
